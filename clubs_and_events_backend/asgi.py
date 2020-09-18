70c536d70a9e5fffce47f3e6f58f91ffb527fa3f
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2020-09-18 11:50:29 +0700

    📦 Started project

---
+++ clubs_and_events_backend/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for clubs_and_events_backend project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clubs_and_events_backend.settings')
+
+application = get_asgi_application()
