4fd020262bef8f50260175b766fef033be0a8c91
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2020-09-20 10:30:38 +0700

    💡 Removed commented code

--- clubs_and_events/settings.py
+++ clubs_and_events/settings.py
@@ -76,10 +76,6 @@ WSGI_APPLICATION = 'clubs_and_events.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': list(open('KEY.txt'))[1].replace('\n', ''),
