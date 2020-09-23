a83740197616a09700c6de781a78d8db76b249d0
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2020-09-23 17:14:52 +0700

    ✨ Enabled CORS

--- clubs_and_events/settings.py
+++ clubs_and_events/settings.py
@@ -44,9 +44,13 @@ INSTALLED_APPS = [
     'core',
     'membership',
     'user',
+    'corsheaders',
 ]
 
+CORS_ORIGIN_ALLOW_ALL=True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,5 +136,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Auth User Model
+# Custom user model for authentication
 
 AUTH_USER_MODEL = 'user.User'
