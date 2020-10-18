c596242e4596158372eb303e55f5f8c4a4e4e3f4
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2020-10-18 16:33:23 +0700

    ✨ Added club approval document generator
    🗃 Added model
    🍱 Added template
    ✨ Added Django Admin
    📌 Updated requirements.txt
    ⚠️ Missing serializers and views
    ⚠️ Needs to make Microsoft Word file generating more neat

---
+++ generator/generate_docx.py
@@ -0,0 +1,105 @@
+import os
+from datetime import datetime
+
+from docx import Document
+
+from clubs_and_events.settings import STORAGE_BASE_DIR
+from membership.models import Membership
+
+
+def generate_docx(file_name, generated_file_name=None, club=None, advisor=None, objective=str(), objective_list=tuple(),
+                  room=str(), schedule=str(), plan_list=tuple(), merit=str(), save=True):
+    document = Document('generator/docx/{}'.format(file_name))
+
+    if generated_file_name is None:
+        generated_file_name = 'generated-' + file_name
+
+    if not os.path.exists('{}/generated_docx'.format(STORAGE_BASE_DIR)):
+        os.mkdir('{}/generated_docx'.format(STORAGE_BASE_DIR))
+    if not os.path.exists('{}/generated_docx/{}'.format(STORAGE_BASE_DIR, club.id)):
+        os.mkdir('{}/generated_docx/{}'.format(STORAGE_BASE_DIR, club.id))
+
+    path = '{}/generated_docx/{}/{}'.format(STORAGE_BASE_DIR, club.id, generated_file_name)
+
+    data = {
+        'date': get_date(),
+        'club_name': club.name_th.replace('ชุมนุม', ''),
+        'student_committee_president': '',
+        'president': get_club_president(club),
+        'advisor': advisor.name,
+        'staff_list': get_staff_list(club),
+        'objective': objective,
+        'objective_list': objective_list,
+        'room': room,
+        'schedule': schedule,
+        'plan_list': plan_list,
+        'merit': merit,
+        'member_list': get_member_list(club)
+    }
+
+    for paragraph in document.paragraphs:
+        for inline in paragraph.runs:
+            for j in data:
+                if not isinstance(data[j], list):
+                    inline.text = inline.text.replace('{%s}' % j, data[j])
+                else:
+                    inline.text = inline.text.replace(
+                        '{%s}' % j,
+                        '\n'.join(['\t{}. {}'.format(
+                            i + 1, data[j][i].replace('\n', '').replace('\r', '')
+                        ) for i in range(len(data[j]))])
+                    )
+
+    for table in document.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                for paragraph in cell.paragraphs:
+                    for inline in paragraph.runs:
+                        for j in data:
+                            if not isinstance(data[j], list):
+                                inline.text = inline.text.replace('{%s}' % j, data[j])
+
+    if save:
+        document.save(path)
+
+    return document
+
+
+def get_date():
+    today = datetime.now().date()
+    months = {
+        1: 'มกราคม', 2: 'กุมภาพันธ์', 3: 'มีนาคม', 4: 'เมษายน', 5: 'พฤษภาคม', 6: 'มิถุนายน',
+        7: 'กรกฎาคม', 8: 'สิงหาคม', 9: 'กันยายน', 10: 'ตุลาคม', 11: 'พฤศจิกายน', 12: 'ธันวาคม',
+    }
+
+    return '{} {} {}'.format(today.day, months[today.month], today.year + 543)
+
+
+def get_club_president(club):
+    membership = Membership.objects.get(community_id=club.id, position=3, status='A')
+
+    return membership.user.name
+
+
+def get_staff_list(club):
+    memberships = Membership.objects.filter(
+        community_id=club.id, position__in=(1, 2, 3), status='A'
+    ).order_by('-position')
+
+    staff_list = list()
+
+    for i in memberships:
+        if i.position == 3:
+            staff_list.append('{}\tรหัสนักศึกษา {}\tประธานชุมนุม'.format(i.user.name, i.user.username.replace('it', '')))
+        elif i.position == 2:
+            staff_list.append('{}\tรหัสนักศึกษา {}\tรองประธานชุมนุม'.format(i.user.name, i.user.username.replace('it', '')))
+        elif i.position == 1:
+            staff_list.append('{}\tรหัสนักศึกษา {}\tกรรมการ'.format(i.user.name, i.user.username.replace('it', '')))
+
+    return staff_list
+
+
+def get_member_list(club):
+    memberships = Membership.objects.filter(community_id=club.id, status='A').order_by('-position')
+
+    return [i.user.name for i in memberships]
