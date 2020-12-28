7d46ec390449a212fb78ee00605e14729a644aca
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2020-12-28 13:54:25 +0700

    ✅ Updated comment API tests

--- asset/tests/tests_comment_api.py
+++ asset/tests/tests_comment_api.py
@@ -10,17 +10,17 @@ from rest_framework.test import APITestCase
 
 from asset.models import Comment
 from community.models import Club, Event, CommunityEvent
+from core.utils.general import get_random_string
+from membership.models import Membership
 
 import datetime
-
-from core.utils.general import get_random_string
 
 
 class CommentAPITest(APITestCase):
     ''' Comment API test '''
     def setUp(self):
         ''' Set up '''
-        self.user = get_user_model().objects.create_user(username='user', password='12345678', name='User')
+        self.user_01 = get_user_model().objects.create_user(username='user_01', password='12345678', name='User One')
 
         self.club_public = Club.objects.create(
             name_th='ชุมนุมทดสอบความคิดเห็น สาธารณะ', name_en='Comment Testing Club (Public)',
@@ -45,7 +45,7 @@ class CommentAPITest(APITestCase):
 
     def test_list_comment_event_authenticated(self):
         ''' Test list comment on events while authenticated '''
-        self.client.login(username='user', password='12345678')
+        self.client.login(username='user_01', password='12345678')
 
         Comment.objects.create(text='Greetings!', written_by='Guest', event_id=self.event_public.id)
         Comment.objects.create(text='Hello!', written_by='Guest', event_id=self.event_public.id)
@@ -69,7 +69,7 @@ class CommentAPITest(APITestCase):
 
     def test_list_comment_community_event_authenticated(self):
         ''' Test list comment on community events while authenticated '''
-        self.client.login(username='user', password='12345678')
+        self.client.login(username='user_01', password='12345678')
 
         self._test_list_comment_community_event(
             is_publicly_visible=True, created_under_id=self.club_public.id, expected_length=1
@@ -114,9 +114,297 @@ class CommentAPITest(APITestCase):
         )
 
         comment = Comment.objects.create(text='Greetings!', written_by='Guest', event_id=community_event.id)
-
         response = self.client.get('/api/asset/comment/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), expected_length)
 
         comment.delete()
+
+    def test_retrieve_comment_event_authenticated(self):
+        ''' Test retrieve comment on events while authenticated '''
+        self.client.login(username='user_01', password='12345678')
+
+        comment = Comment.objects.create(text='Greetings!', written_by='Guest', event_id=self.event_public.id)
+        response = self.client.get('/api/asset/comment/{}/'.format(comment.id))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        comment = Comment.objects.create(text='Hi!', written_by='Guest', event_id=self.event_private.id)
+        response = self.client.get('/api/asset/comment/{}/'.format(comment.id))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.client.logout()
+
+    def test_retrieve_comment_event_unauthenticated(self):
+        ''' Test retrieve comment on events while unauthenticated '''
+        comment = Comment.objects.create(text='Greetings!', written_by='Guest', event_id=self.event_public.id)
+        response = self.client.get('/api/asset/comment/{}/'.format(comment.id))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        comment = Comment.objects.create(text='Hi!', written_by='Guest', event_id=self.event_private.id)
+        response = self.client.get('/api/asset/comment/{}/'.format(comment.id))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_retrieve_comment_community_event_authenticated(self):
+        ''' Test retrieve comment on community events while authenticated '''
+        self.client.login(username='user_01', password='12345678')
+
+        self._test_retrieve_comment_community_event(
+            is_publicly_visible=True, created_under_id=self.club_public.id, allows_retrieve=True
+        )
+        self._test_retrieve_comment_community_event(
+            is_publicly_visible=True, created_under_id=self.club_private.id, allows_retrieve=True
+        )
+        self._test_retrieve_comment_community_event(
+            is_publicly_visible=False, created_under_id=self.club_public.id, allows_retrieve=True
+        )
+        self._test_retrieve_comment_community_event(
+            is_publicly_visible=False, created_under_id=self.club_private.id, allows_retrieve=True
+        )
+
+        self.client.logout()
+
+    def test_retrieve_comment_community_event_unauthenticated(self):
+        ''' Test retrieve comment on community events while unauthenticated '''
+        self._test_retrieve_comment_community_event(
+            is_publicly_visible=True, created_under_id=self.club_public.id, allows_retrieve=True
+        )
+        self._test_retrieve_comment_community_event(
+            is_publicly_visible=True, created_under_id=self.club_private.id, allows_retrieve=False
+        )
+        self._test_retrieve_comment_community_event(
+            is_publicly_visible=False, created_under_id=self.club_public.id, allows_retrieve=False
+        )
+        self._test_retrieve_comment_community_event(
+            is_publicly_visible=False, created_under_id=self.club_private.id, allows_retrieve=False
+        )
+
+    def _test_retrieve_comment_community_event(self, is_publicly_visible=True, created_under_id=int(),
+                                               allows_retrieve=True):
+        ''' Test retrieve comment on community events '''
+        community_event = CommunityEvent.objects.create(
+            name_th='กิจกรรมชุมนุมทดสอบความคิดเห็น พิเศษ - {}'.format(get_random_string(length=16)),
+            name_en='Comment Testing Club Event (Special) - {}'.format(get_random_string(length=16)),
+            is_approved=True, location='L207 IT KMITL',
+            start_date=datetime.date(2020, 12, 1), end_date=datetime.date(2020, 12, 2),
+            start_time=datetime.time(9, 0, 0), end_time=datetime.time(17, 0, 0),
+            is_publicly_visible=is_publicly_visible, created_under_id=created_under_id,
+            allows_outside_participators=True
+        )
+
+        comment = Comment.objects.create(text='Greetings!', written_by='Guest', event_id=community_event.id)
+        response = self.client.get('/api/asset/comment/{}/'.format(comment.id))
+
+        if allows_retrieve:
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+        else:
+            self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        comment.delete()
+
+    def test_create_comment_event_authenticated(self):
+        ''' Test create comment on events while authenticated '''
+        self.client.login(username='user_01', password='12345678')
+
+        response = self.client.post('/api/asset/comment/', {
+            'text': 'Greetings!',
+            'written_by': 'Guest',
+            'event': self.event_public.id
+        })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        response = self.client.post('/api/asset/comment/', {
+            'text': 'Greetings!',
+            'written_by': 'Guest',
+            'event': self.event_private.id
+        })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        self.client.logout()
+
+    def test_create_comment_event_unauthenticated(self):
+        ''' Test create comment on events while unauthenticated '''
+        response = self.client.post('/api/asset/comment/', {
+            'text': 'Greetings!',
+            'written_by': 'Guest',
+            'event': self.event_public.id
+        })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        response = self.client.post('/api/asset/comment/', {
+            'text': 'Greetings!',
+            'written_by': 'Guest',
+            'event': self.event_private.id
+        })
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_comment_community_event_authenticated_member(self):
+        ''' Test create comment on community events while authenticated and a member '''
+        self.client.login(username='user_01', password='12345678')
+
+        self._test_create_comment_community_event(
+            is_publicly_visible=True, created_under_id=self.club_public.id, allows_outside_participators=True,
+            add_member='user_01', allows_retrieve=True
+        )
+        self._test_create_comment_community_event(
+            is_publicly_visible=True, created_under_id=self.club_public.id, allows_outside_participators=False,
+            add_member='user_01', allows_retrieve=True
+        )
+        self._test_create_comment_community_event(
+            is_publicly_visible=True, created_under_id=self.club_private.id, allows_outside_participators=True,
+            add_member='user_01', allows_retrieve=True
+        )
+        self._test_create_comment_community_event(
+            is_publicly_visible=True, created_under_id=self.club_private.id, allows_outside_participators=False,
+            add_member='user_01', allows_retrieve=True
+        )
+        self._test_create_comment_community_event(
+            is_publicly_visible=False, created_under_id=self.club_public.id, allows_outside_participators=True,
+            add_member='user_01', allows_retrieve=True
+        )
+        self._test_create_comment_community_event(
+            is_publicly_visible=False, created_under_id=self.club_public.id, allows_outside_participators=False,
+            add_member='user_01', allows_retrieve=True
+        )
+        self._test_create_comment_community_event(
+            is_publicly_visible=False, created_under_id=self.club_private.id, allows_outside_participators=True,
+            add_member='user_01', allows_retrieve=True
+        )
+        self._test_create_comment_community_event(
+            is_publicly_visible=False, created_under_id=self.club_private.id, allows_outside_participators=False,
+            add_member='user_01', allows_retrieve=True
+        )
+
+        self.client.logout()
+
+    def test_create_comment_community_event_authenticated_non_member(self):
+        ''' Test create comment on community events while authenticated and a member '''
+        self.client.login(username='user_01', password='12345678')
+
+        self._test_create_comment_community_event(
+            is_publicly_visible=True, created_under_id=self.club_public.id, allows_outside_participators=True,
+            allows_retrieve=True
+        )
+        self._test_create_comment_community_event(
+            is_publicly_visible=True, created_under_id=self.club_public.id, allows_outside_participators=False,
+            allows_retrieve=False
+        )
+        self._test_create_comment_community_event(
+            is_publicly_visible=True, created_under_id=self.club_private.id, allows_outside_participators=True,
+            allows_retrieve=True
+        )
+        self._test_create_comment_community_event(
+            is_publicly_visible=True, created_under_id=self.club_private.id, allows_outside_participators=False,
+            allows_retrieve=False
+        )
+        self._test_create_comment_community_event(
+            is_publicly_visible=False, created_under_id=self.club_public.id, allows_outside_participators=True,
+            allows_retrieve=True
+        )
+        self._test_create_comment_community_event(
+            is_publicly_visible=False, created_under_id=self.club_public.id, allows_outside_participators=False,
+            allows_retrieve=False
+        )
+        self._test_create_comment_community_event(
+            is_publicly_visible=False, created_under_id=self.club_private.id, allows_outside_participators=True,
+            allows_retrieve=True
+        )
+        self._test_create_comment_community_event(
+            is_publicly_visible=False, created_under_id=self.club_private.id, allows_outside_participators=False,
+            allows_retrieve=False
+        )
+
+        self.client.logout()
+
+    def test_create_comment_community_event_unauthenticated(self):
+        ''' Test create comment on community events while unauthenticated '''
+        self._test_create_comment_community_event(
+            is_publicly_visible=True, created_under_id=self.club_public.id, allows_outside_participators=True,
+            allows_retrieve=True
+        )
+        self._test_create_comment_community_event(
+            is_publicly_visible=True, created_under_id=self.club_public.id, allows_outside_participators=False,
+            allows_retrieve=False
+        )
+        self._test_create_comment_community_event(
+            is_publicly_visible=True, created_under_id=self.club_private.id, allows_outside_participators=True,
+            allows_retrieve=False
+        )
+        self._test_create_comment_community_event(
+            is_publicly_visible=True, created_under_id=self.club_private.id, allows_outside_participators=False,
+            allows_retrieve=False
+        )
+        self._test_create_comment_community_event(
+            is_publicly_visible=False, created_under_id=self.club_public.id, allows_outside_participators=True,
+            allows_retrieve=False
+        )
+        self._test_create_comment_community_event(
+            is_publicly_visible=False, created_under_id=self.club_public.id, allows_outside_participators=False,
+            allows_retrieve=False
+        )
+        self._test_create_comment_community_event(
+            is_publicly_visible=False, created_under_id=self.club_private.id, allows_outside_participators=True,
+            allows_retrieve=False
+        )
+        self._test_create_comment_community_event(
+            is_publicly_visible=False, created_under_id=self.club_private.id, allows_outside_participators=False,
+            allows_retrieve=False
+        )
+
+    def _test_create_comment_community_event(self, is_publicly_visible=True, created_under_id=int(),
+                                             allows_outside_participators=True, add_member=None, allows_retrieve=True):
+        ''' Test create comment on community events '''
+        community_event = CommunityEvent.objects.create(
+            name_th='กิจกรรมชุมนุมทดสอบความคิดเห็น พิเศษ - {}'.format(get_random_string(length=16)),
+            name_en='Comment Testing Club Event (Special) - {}'.format(get_random_string(length=16)),
+            is_approved=True, location='L207 IT KMITL',
+            start_date=datetime.date(2020, 12, 1), end_date=datetime.date(2020, 12, 2),
+            start_time=datetime.time(9, 0, 0), end_time=datetime.time(17, 0, 0),
+            is_publicly_visible=is_publicly_visible, created_under_id=created_under_id,
+            allows_outside_participators=allows_outside_participators
+        )
+
+        if isinstance(add_member, str):
+            Membership.objects.create(
+                user_id=get_user_model().objects.get(username=add_member).id, community_id=community_event.id
+            )
+
+        response = self.client.post('/api/asset/comment/', {
+            'text': 'Greetings!',
+            'written_by': 'Guest',
+            'event': community_event.id
+        })
+
+        if allows_retrieve:
+            self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        else:
+            self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_update_comment(self):
+        ''' Test update comment '''
+        self.client.login(username='user_01', password='12345678')
+
+        comment = Comment.objects.create(text='Greetings!', written_by='Guest', event_id=self.event_public.id)
+        response = self.client.put('/api/asset/comment/{}/'.format(comment.id), {
+            'text': 'Hello!'
+        })
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        self.assertEqual(Comment.objects.get(pk=comment.id).text, 'Greetings!')
+
+        comment = Comment.objects.create(text='Greetings!', written_by='Guest', event_id=self.event_public.id)
+        response = self.client.patch('/api/asset/comment/{}/'.format(comment.id), {
+            'text': 'Hello!'
+        })
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        self.assertEqual(Comment.objects.get(pk=comment.id).text, 'Greetings!')
+
+        self.client.logout()
+
+    def test_delete_comment(self):
+        ''' Test delete comment '''
+        self.client.login(username='user_01', password='12345678')
+
+        comment = Comment.objects.create(text='Greetings!', written_by='Guest', event_id=self.event_public.id)
+        response = self.client.delete('/api/asset/comment/{}/'.format(comment.id))
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        self.client.logout()
