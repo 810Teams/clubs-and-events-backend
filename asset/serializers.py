e496823367f8e2753e5e8fa341c17c2282731e80
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2021-05-25 13:45:27 +0700

    🐛 Fixed incorrect error message field
    🐛 Fixed incorrect error message field on submitting too many comments in a short time from event error field to non_field_errors

--- asset/serializers.py
+++ asset/serializers.py
@@ -284,7 +284,7 @@ class CommentSerializer(serializers.ModelSerializer):
             target_comment = user_comments[len(user_comments) - COMMENT_LIMIT_PER_INTERVAL]
             if target_comment.created_at + COMMENT_INTERVAL_TIME > timezone.now():
                 add_error_message(
-                    errors, key='event', message='Comments limit reached, please try again after a while.'
+                    errors, message='Comments limit reached, please try again after a while.'
                 )
 
         # Prevent comment creation in non-active community
